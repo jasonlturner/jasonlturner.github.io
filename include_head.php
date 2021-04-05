93bb0be90394de16b8849b872d7d02cb7ae5b283
jasonltorchinsky <jason.torchinsky@wisc.edu>
2021-04-05 15:33:32 -0500

    Tests including server-side with HTML.

---
+++ include_head.php
@@ -0,0 +1,20 @@
+<header id="header" class="fixed-top">
+  <div class="container">
+
+    <div class="logo float-left">
+      <h1 class="text-light"><a href="index.html"><span></span></a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+    </div>
+
+    <nav class="nav-menu float-right d-none d-lg-block">
+      <ul>
+        <li class="active"><a href="index.html">Home</a></li>
+        <li><a href="studentresources.html">Student Resources</a></li>
+        <li><a href="works.html">Works</a></li>
+        <li><a href="torchinsky_jason_cv_2020_12.pdf">My CV</a></li>
+      </ul>
+    </nav><!-- .nav-menu -->
+
+  </div>
+</header>
